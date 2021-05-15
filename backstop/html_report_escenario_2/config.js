498145f1145c070db8528c46c238da02c7cf337c
Rafael David Matiz Cortes <r.matiz@uniandes.edu.co>
2021-05-14 23:22:39 -0500

    Análisis regresión visual backstop 5 escenarios

---
+++ backstop/html_report_escenario_2/config.js
@@ -0,0 +1,174 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_ghost_Escenario_2_paso_1_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20210514-220739\\backstop_ghost_Escenario_2_paso_1_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_ghost_Escenario_2_paso_1_0_document_0_default.png",
+        "label": "Escenario 2 paso 1",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 8,
+        "url": "http://127.0.0.1:8080/",
+        "referenceUrl": "http://127.0.0.1:8080/",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_ghost_Escenario_2_paso_2_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20210514-220739\\backstop_ghost_Escenario_2_paso_2_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_ghost_Escenario_2_paso_2_0_document_0_default.png",
+        "label": "Escenario 2 paso 2",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 8,
+        "url": "http://127.0.0.1:8080/",
+        "referenceUrl": "http://127.0.0.1:8080/",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_ghost_Escenario_2_paso_3_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20210514-220739\\backstop_ghost_Escenario_2_paso_3_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_ghost_Escenario_2_paso_3_0_document_0_default.png",
+        "label": "Escenario 2 paso 3",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 8,
+        "url": "http://127.0.0.1:8080/",
+        "referenceUrl": "http://127.0.0.1:8080/",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_ghost_Escenario_2_paso_4_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20210514-220739\\backstop_ghost_Escenario_2_paso_4_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_ghost_Escenario_2_paso_4_0_document_0_default.png",
+        "label": "Escenario 2 paso 4",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 8,
+        "url": "http://127.0.0.1:8080/",
+        "referenceUrl": "http://127.0.0.1:8080/",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_ghost_Escenario_2_paso_5_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20210514-220739\\backstop_ghost_Escenario_2_paso_5_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_ghost_Escenario_2_paso_5_0_document_0_default.png",
+        "label": "Escenario 2 paso 5",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 8,
+        "url": "http://127.0.0.1:8080/",
+        "referenceUrl": "http://127.0.0.1:8080/",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_ghost_Escenario_2_paso_6_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20210514-220739\\backstop_ghost_Escenario_2_paso_6_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_ghost_Escenario_2_paso_6_0_document_0_default.png",
+        "label": "Escenario 2 paso 6",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 8,
+        "url": "http://127.0.0.1:8080/",
+        "referenceUrl": "http://127.0.0.1:8080/",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_ghost_Escenario_2_paso_7_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20210514-220739\\backstop_ghost_Escenario_2_paso_7_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_ghost_Escenario_2_paso_7_0_document_0_default.png",
+        "label": "Escenario 2 paso 7",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 8,
+        "url": "http://127.0.0.1:8080/",
+        "referenceUrl": "http://127.0.0.1:8080/",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "backstop_ghost"
+});
---
+++ backstop/html_report_escenario_3/config.js
@@ -0,0 +1,259 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_ghost_Escenario_1_paso_1_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20210514-224128\\backstop_ghost_Escenario_1_paso_1_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_ghost_Escenario_1_paso_1_0_document_0_default.png",
+        "label": "Escenario 1 paso 1",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 8,
+        "url": "http://127.0.0.1:8081/",
+        "referenceUrl": "http://127.0.0.1:8080/",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 73
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_ghost_Escenario_1_paso_2_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20210514-224128\\backstop_ghost_Escenario_1_paso_2_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_ghost_Escenario_1_paso_2_0_document_0_default.png",
+        "label": "Escenario 1 paso 2",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 8,
+        "url": "http://127.0.0.1:8081/",
+        "referenceUrl": "http://127.0.0.1:8080/",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 75
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_ghost_Escenario_1_paso_3_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20210514-224128\\backstop_ghost_Escenario_1_paso_3_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_ghost_Escenario_1_paso_3_0_document_0_default.png",
+        "label": "Escenario 1 paso 3",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 8,
+        "url": "http://127.0.0.1:8081/",
+        "referenceUrl": "http://127.0.0.1:8080/",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "5.26",
+          "analysisTime": 69
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_ghost_Escenario_1_paso_4_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20210514-224128\\backstop_ghost_Escenario_1_paso_4_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_ghost_Escenario_1_paso_4_0_document_0_default.png",
+        "label": "Escenario 1 paso 4",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 8,
+        "url": "http://127.0.0.1:8081/",
+        "referenceUrl": "http://127.0.0.1:8080/",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "2.92",
+          "analysisTime": 63
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_ghost_Escenario_1_paso_5_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20210514-224128\\backstop_ghost_Escenario_1_paso_5_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_ghost_Escenario_1_paso_5_0_document_0_default.png",
+        "label": "Escenario 1 paso 5",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 8,
+        "url": "http://127.0.0.1:8081/",
+        "referenceUrl": "http://127.0.0.1:8080/",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "4.60",
+          "analysisTime": 76
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_ghost_Escenario_1_paso_6_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20210514-224128\\backstop_ghost_Escenario_1_paso_6_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_ghost_Escenario_1_paso_6_0_document_0_default.png",
+        "label": "Escenario 1 paso 6",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 8,
+        "url": "http://127.0.0.1:8081/",
+        "referenceUrl": "http://127.0.0.1:8080/",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "10.38",
+          "analysisTime": 69
+        },
+        "diffImage": "..\\bitmaps_test\\20210514-224128\\failed_diff_backstop_ghost_Escenario_1_paso_6_0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_ghost_Escenario_1_paso_7_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20210514-224128\\backstop_ghost_Escenario_1_paso_7_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_ghost_Escenario_1_paso_7_0_document_0_default.png",
+        "label": "Escenario 1 paso 7",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 8,
+        "url": "http://127.0.0.1:8081/",
+        "referenceUrl": "http://127.0.0.1:8080/",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "4.94",
+          "analysisTime": 83
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_ghost_Escenario_1_paso_8_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20210514-224128\\backstop_ghost_Escenario_1_paso_8_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_ghost_Escenario_1_paso_8_0_document_0_default.png",
+        "label": "Escenario 1 paso 8",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 8,
+        "url": "http://127.0.0.1:8081/",
+        "referenceUrl": "http://127.0.0.1:8080/",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "3.07",
+          "analysisTime": 73
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_ghost_Escenario_1_paso_9_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20210514-224128\\backstop_ghost_Escenario_1_paso_9_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_ghost_Escenario_1_paso_9_0_document_0_default.png",
+        "label": "Escenario 1 paso 9",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 8,
+        "url": "http://127.0.0.1:8081/",
+        "referenceUrl": "http://127.0.0.1:8080/",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "10.55",
+          "analysisTime": 81
+        },
+        "diffImage": "..\\bitmaps_test\\20210514-224128\\failed_diff_backstop_ghost_Escenario_1_paso_9_0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_ghost_Escenario_1_paso_10_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20210514-224128\\backstop_ghost_Escenario_1_paso_10_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_ghost_Escenario_1_paso_10_0_document_0_default.png",
+        "label": "Escenario 1 paso 10",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 8,
+        "url": "http://127.0.0.1:8081/",
+        "referenceUrl": "http://127.0.0.1:8080/",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "10.59",
+          "analysisTime": 50
+        },
+        "diffImage": "..\\bitmaps_test\\20210514-224128\\failed_diff_backstop_ghost_Escenario_1_paso_10_0_document_0_default.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "backstop_ghost"
+});

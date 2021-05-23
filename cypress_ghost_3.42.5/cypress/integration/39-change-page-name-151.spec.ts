20937b53978959cab35a891e42ee01fe712e8976
Hector Varon Bonilla <h.varon@uniandes.edu.co>
2021-05-23 17:03:40 -0500

    Pruebas con Pool de datos pseudo aleatorio dinámico

---
+++ cypress_ghost_3.42.5/cypress/integration/39-change-page-name-151.spec.ts
@@ -0,0 +1,77 @@
+import { Login } from "../page-objects/login";
+import { SideBar } from "../page-objects/side-bar";
+import { SettingsPage } from "../page-objects/settings-page";
+import * as faker from "faker";
+import { should } from "chai";
+const login = new Login();
+const sideBar = new SideBar();
+const settingsPage = new SettingsPage();
+
+const cookieSessionName =
+  Cypress.env("cookieSessionName") || "ghost-admin-api-session";
+
+describe("Should login and create a post with title succesfully", () => {
+  Cypress.on("uncaught:exception", (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false;
+  });
+
+  const newTitle = faker.lorem.words();
+  let tempURL: string;
+
+  let datetime;
+  before(() => {
+    datetime = new Date().toISOString().replace(/:/g, ".");
+    cy.request(
+        {
+            method : 'GET',
+            url : 'https://my.api.mockaroo.com/site_config.json?key=872fc0c0',
+            headers : {
+                'content-type' : 'application/json'
+            },
+            body : {
+                'newTitle' : 'site_name_151'
+            }
+        }).then((response)  => {
+            expect(response).property('status').to.equal(200)
+            expect(response.body).property('site_name_151').to.not.be.oneOf([null, ""])
+            const body = (response.body)
+            newTitle = body['site_name_151']
+        });
+  });
+
+  beforeEach(() => {
+    Cypress.Cookies.preserveOnce(cookieSessionName);
+  });
+
+  it("should login the user", () => {
+    login.visit();
+    login.loginWithEnvUser();
+    cy.url().should("include", "/#/site");
+    cy.screenshot(`${datetime}/image-1`);
+  });
+
+  it("should go to settings", () => {
+    if (sideBar.checkIfComponentExists()) {
+      cy.log("theres sidebar");
+      sideBar.goToGeneralSettings();
+      cy.screenshot(`${datetime}/image-2`);
+    }
+  });
+
+  it("should change name of the website", () => {
+    settingsPage
+      .expandTitleAndDescription()
+      cy.screenshot(`${datetime}/image-3`);
+      settingsPage
+      .fillNameSite(newTitle)
+      .saveSettings();
+  });
+
+  it("should go to site", () => {
+    sideBar.goToSite();
+    cy.screenshot(`${datetime}/image-4`);
+  });
+
+});
